7a29165ac17b753cbd3cdb4bf7db2d5aed7dadd9
apple <test@example.com>
2021-06-11 13:54:30 +0900

    top_controller_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   root to: 'top#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :health_check, only: [:index]
